43e5f1d464af1d72aea355bd850fb4333ccd2299
sklarz <sklarz@bgu.ac.il>
2019-02-27 12:35:08 +0200

    * Updated for NAR submission

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name                = 'NeatSeq-Flow-tutorial',
-    version             = '1.1.0',
+    version             = '1.6.0',
     author              = 'Liron Levin and Menachem Sklarz',
     author_email        = 'levinl@post.bgu.ac.il',
     maintainer          = 'Menachem Sklarz',
